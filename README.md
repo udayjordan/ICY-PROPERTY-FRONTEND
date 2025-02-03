93804859b54ec463609b4ef3744eda849250a95d
Uday_jordan <80660710+udayjordan@users.noreply.github.com>
2025-02-03 19:19:25 +0530

    files added

---
+++ README.md
@@ -0,0 +1,27 @@
+
+
+### Features
+* Landing Page
+* Property Listing Page
+* Property Details Page
+* Agency Page
+* Blog Page
+* Signup and Login Pages
+* User Authentication
+* Search Query
+* Highly responsive and aesthetic design
+* FAQs
+
+### Dependencies/Tools
+* React
+* Redux Toolkit
+* React Router 5
+* Tailwindcss
+* Firebase
+* React Icons
+
+### APIs
+* Firebase Auth REST API
+* Bayut API  [Real Estate]
+* Newscatcher API [Global News]
+
---
+++ tailwind.config.js
@@ -0,0 +1,72 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      xm: "200px",
+      sm: "380px",
+      md: "768px",
+      lg: "976px",
+      xl: "1200px",
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      black: "#00040a",
+      blue: "#3A76DA",
+      liteBlue: "#ffff00",
+      silver: "#f0f3f7",
+      darkBg: "#212121",
+      darkCard: "#fff",
+      silverLite: "#fafbfc",
+      ash: "#666565",
+      yellow: "#FED018",
+      lightyellow: "#FFF9BF",
+      green: "#88C273",
+      amber1: "#FED018",
+    },
+    extend: {
+      fontFamily: {
+        Poppins: ["'Poppins'", "'sans - serif'"],
+      },
+      animation: {
+        slideup: "slideup 1s ease-in-out",
+        slidedown: "slidedown 1s ease-in-out",
+        slideleft: "slideleft 1s ease-in-out",
+        slideright: "slideright 1s ease-in-out",
+        wave: "wave 1.2s linear infinite",
+        slowfade: "slowfade 2.2s ease-in-out",
+      },
+      keyframes: {
+        slowfade: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        slideup: {
+          from: { opacity: 0, transform: "translateY(25%)" },
+          to: { opacity: 1, transform: "none" },
+        },
+        slidedown: {
+          from: { opacity: 0, transform: "translateY(-25%)" },
+          to: { opacity: 1, transform: "none" },
+        },
+        slideleft: {
+          from: { opacity: 0, transform: "translateX(-20px)" },
+          to: { opacity: 1, transform: "translateX(0)" },
+        },
+        slideright: {
+          from: { opacity: 0, transform: "translateX(20px)" },
+          to: { opacity: 1, transform: "translateX(0)" },
+        },
+        wave: {
+          "0%": { transform: "scale(0)" },
+          "50%": { transform: "scale(1)" },
+          "100%": { transform: "scale(0)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
